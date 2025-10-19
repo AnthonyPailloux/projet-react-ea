dbfbc864d99bb883f78e9ad0d38661fd903689de
AnthonyPailloux <anthonypailloux@outlook.com>
2025-10-19 16:55:48 +0200

    next commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Facts from './pages/Facts'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Exo sur l'API "veilleapi"</h1>
+    <Facts/>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/pages/View.jsx
@@ -0,0 +1,12 @@
+
+
+
+function View() {
+    return (
+        <>
+            <h2>My view page</h2>
+        </>
+    )
+}
+
+export default View
